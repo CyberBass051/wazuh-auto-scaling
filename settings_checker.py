62ac9f0ff7c95f7b3ac604e6f77021e0c2fec160
CyberBass051 <pb05129000@tutamail.com>
2026-05-05 02:05:08 +0200

    Added security group setting checker logic inside 'setting_checker.py'

--- settings_checker.py
+++ settings_checker.py
@@ -6,6 +6,8 @@ from logging.handlers import RotatingFileHandler
 import tempfile
 import argparse
 import re
+import json
+import sys
 
 LOG_DIR = "logs"
 os.makedirs(LOG_DIR, exist_ok=True)
@@ -13,7 +15,7 @@ LOG_FILE = os.path.join(LOG_DIR, "settings_checker.log")
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Check settings file")
-    parser.add_argument("-f", "--file", help="Settings file to check", required=True)
+    parser.add_argument("-f", "--files",  nargs='+', help="Settings file to check", required=True)
     parser.add_argument("--dry-run", help="Don't actually change anything", action="store_true")
     return parser.parse_args()
 
@@ -38,64 +40,109 @@ def atomic_write(filename, content):
     # Atomically replace the original file
     os.replace(tmp_filename, filename)
 
-def check_settings(file: str, dry_run: bool = False) -> None:
-    try:
-        if file == 'alb.tf':
-            pattern_to_check = r'(drop_invalid_header_fields\s+=\s+)(true|false)'
-            with open(file, 'r') as f:
-                content = f.read()
-                match = re.search(pattern_to_check, content)
-                if not match:
-                    if not dry_run:
-                        # Perform the atomic write
-                        logger.info("Pattern not found in %s. Adding it.", file)
-                        new_content = re.sub(r'(resource "aws_lb" ".*" {)', 
-                                         r'\1\n  drop_invalid_header_fields = true\n  enable_deletion_protection = false # Set to true for production!' , content)
-                        atomic_write(file, new_content)
-                    else:
-                        logger.info("Dry run: would add pattern to %s", file)
-                
-                elif match.group(2) == 'false':
-                    logger.info("Drop_invalid_header_fields is set to false in %s.", file)
-                    if not dry_run:
-                        logger.info("Setting drop_invalid_header_fields to true in %s.", file)
-                        new_content = re.sub(pattern_to_check, r'\1true', content)
-                        atomic_write(file, new_content)
-                    else:
-                        logger.info("Dry run: would set drop_invalid_header_fields to true in %s", file)
+def check_settings(files: str, dry_run: bool = False) -> None:
     
-        elif file == 'ec2.tf':
-            with open(file, 'r') as f:
-                content = f.read()
-                pattern_to_check = r'(metadata_options)(\s+{\s+http_endpoint\s+=\s+")(enabled|disabled)("\s+http_tokens\s+=\s+"required"\s+http_put_response_hop_limit\s+=\s+)(1|\d?)(\s+})'
-                match = re.search(pattern_to_check, content, re.DOTALL)
-                if not match:
-                    if not dry_run:
-                        # Perform the atomic write
-                        logger.info("Pattern 'metadata_options' not found in %s. Adding it.", file)
-                        new_content = re.sub(r'(resource ".*" ".*" {)',
-                                         r'\1\n    metadata_options {\n      http_endpoint = "enabled"\n      http_tokens   = "required"\n      http_put_response_hop_limit = 1\n    }\n' , content)
-                        atomic_write(file, new_content)
-                    else:
-                        new_content = re.sub(r'(resource ".*" ".*" {)',
-                                         r'\1\n    metadata_options {\n    http_endpoint = "enabled"\n    http_tokens   = "required"\n    http_put_response_hop_limit = 1\n    }\n' , content)
-                        logger.info("Dry run: would add\n%s\nto %s", new_content, file)
-                elif match.group(3) == 'disabled':
-                    if not dry_run:
-                        logger.info("metadata_option's http_endpoint is set to disabled. Setting it to 'enabled'")
-                        new_content = re.sub(pattern_to_check, r'\1\2enabled\4\5 1\6', content)
-                        atomic_write(file, new_content)
-                    else:
-                        new_content = re.sub(pattern_to_check, r'\1\2enabled\4\5 1\6', content)
-                        logger.info("Dry run: would change\n%s\nto\n%s", content, new_content)
-                elif match.group(5) != '1':
-                    if not dry_run:
-                        logger.info("metadata_option's http_put_response_hop_limit is not set to 1. Setting it to 1")
-                        new_content = re.sub(pattern_to_check, r'\1\2\3\4\5 1\6', content)
-                        atomic_write(file, new_content)
-                    else:
-                        new_content = re.sub(pattern_to_check, r'\1\2\3\5 1\6', content)
-                        logger.info("Dry run: would change\n%s\nto\n%s", content, new_content)
+    logger.info("Checking settings file: %s", files)
+    json_report = {
+        "files": [],
+        "summary": {
+            "total_files": 0,
+            "files_with_issues": 0,
+            "issues": []
+        }
+    }
+    try:
+        for file in files:
+            json_report["files"].append({"filename": file, "status": "checked"})
+            json_report["summary"]["total_files"] += 1
+            if file == 'alb.tf':
+                logger.info("Checking alb.tf")
+                pattern_to_check = r'(drop_invalid_header_fields\s+=\s+)(true|false)'
+                with open(file, 'r') as f:
+                    content = f.read()
+                    match = re.search(pattern_to_check, content)
+                    if not match:
+                        json_report["summary"]["issues"].append({"file": file, "issue": "no drop_invalid_header found"})
+                        if not dry_run:
+                            # Perform the atomic write
+                            logger.info("Pattern not found in %s. Adding it.", file)
+                            new_content = re.sub(r'(resource "aws_lb" ".*" {)', 
+                                             r'\1\n  drop_invalid_header_fields = true\n  enable_deletion_protection = false # Set to true for production!' , content)
+                            atomic_write(file, new_content)
+                        else:
+                            logger.info("Dry run: would add pattern to %s", file)
+
+                    elif match.group(2) == 'false':
+                        logger.info("Drop_invalid_header_fields is set to false in %s.", file)
+                        json_report["summary"]["issues"].append({"file": file, "issue": "drop_invalid_header set to disabled"})
+                        if not dry_run:
+                            logger.info("Setting drop_invalid_header_fields to true in %s.", file)
+                            new_content = re.sub(pattern_to_check, r'\1true', content)
+                            atomic_write(file, new_content)
+                        else:
+                            logger.info("Dry run: would set drop_invalid_header_fields to true in %s", file)
+
+            elif file == 'ec2.tf':
+                logger.info("Checking ec2.tf")
+                with open(file, 'r') as f:
+                    content = f.read()
+                    pattern_to_check = r'(metadata_options)(\s+{\s+http_endpoint\s+=\s+")(enabled|disabled)("\s+http_tokens\s+=\s+"required"\s+http_put_response_hop_limit\s+=\s+)(1|\d?)(\s+})'
+                    match = re.search(pattern_to_check, content, re.DOTALL)
+                    if not match:
+                        json_report["summary"]["issues"].append({"file": file, "issue": "No metadata_options found"})
+                        if not dry_run:
+                            # Perform the atomic write
+                            logger.info("Pattern 'metadata_options' not found in %s. Adding it.", file)
+                            new_content = re.sub(r'(resource ".*" ".*" {)',
+                                             r'\1\n    metadata_options {\n      http_endpoint = "enabled"\n      http_tokens   = "required"\n      http_put_response_hop_limit = 1\n    }\n' , content)
+                            atomic_write(file, new_content)
+                        else:
+                            new_content = re.sub(r'(resource ".*" ".*" {)',
+                                             r'\1\n    metadata_options {\n    http_endpoint = "enabled"\n    http_tokens   = "required"\n    http_put_response_hop_limit = 1\n    }\n' , content)
+                            logger.info("Dry run: would add\n%s\nto %s", new_content, file)
+                    elif match.group(3) == 'disabled':
+                        if not dry_run:
+                            json_report["summary"]["issues"].append({"file": file, "issue": "http_endpoint is set to disabled"})
+                            logger.info("metadata_option's http_endpoint is set to disabled. Setting it to 'enabled'")
+                            new_content = re.sub(pattern_to_check, r'\1\2enabled\4\5 1\6', content)
+                            atomic_write(file, new_content)
+                        else:
+                            new_content = re.sub(pattern_to_check, r'\1\2enabled\4\5 1\6', content)
+                            logger.info("Dry run: would change\n%s\nto\n%s", content, new_content)
+                    elif match.group(5) != '1':
+                        json_report["summary"]["issues"].append({"file": file, "issue": "http_put_response_hop_limit is not set to 1"})
+                        if not dry_run:
+                            logger.info("metadata_option's http_put_response_hop_limit is not set to 1. Setting it to 1")
+                            new_content = re.sub(pattern_to_check, r'\1\2\3\4\5 1\6', content)
+                            atomic_write(file, new_content)
+                        else:
+                            new_content = re.sub(pattern_to_check, r'\1\2\3\5 1\6', content)
+                            logger.info("Dry run: would change\n%s\nto\n%s", content, new_content)
+            elif file == 'sg.tf':
+                logger.info("Checking sg.tf")
+                with open(file, 'r') as f:
+                    content = f.read()
+                    pattern_to_check = r'(resource\s+"aws_vpc_security_group_egress_rule)(\s+.*)(cidr_ipv4\s+=\+)("0.0.0.0/0")'
+                    match = re.search(pattern_to_check, content, re.DOTALL)
+                    if not match:
+                        logger.warning("No app egress rule found in %s", file)
+                    elif match.group(4) == "0.0.0.0/0":
+                        json_report["summary"]["issues"].append({"file": file, "issue": "app egress rule allows all"})
+                        logger.warning("Security group egress rule allows all. Please restrict it to a proxy server.")
+                        if not dry_run:
+                            logger.info("Restricting app egress rule to localhost")
+                            new_content = re.sub(pattern_to_check, r'\1\2\3 = "127.0.0.1/32"', content)
+                            atomic_write(file, new_content)
+                        else:
+                            logger.info("Dry run: would change egress rule to localhost")
+            else: 
+                logger.warning("No checks implemented for %s", file)
+                json_report["summary"]["issues"].append({"file": file, "issue": "No checks implemented"})
+
+        with open('report.json', 'w') as f:
+            logger.info("Writing report to report.json")
+            json.dump(json_report, f, indent=4)
+
     except FileNotFoundError:
         logger.error("File %s not found.", file)
     except Exception as e:
@@ -104,7 +151,7 @@ def check_settings(file: str, dry_run: bool = False) -> None:
 def main():
     print("Checking settings file...")
     args = parse_arguments()
-    check_settings(args.file, args.dry_run)
+    check_settings(args.files, args.dry_run)
 
 if __name__ == "__main__":
     main()
